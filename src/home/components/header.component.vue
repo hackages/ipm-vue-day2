0be1ae1f5977d5343bf72a3441f6eb63b290ff1a
Victor <victor@hackages.io>
2018-02-13 22:25:11 +0100

    feat(home): header + menubar

---
+++ src/home/components/header.component.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <img :src="logo" alt="logo"/>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'HfHeader',
+  props: ['logo'],
+  created() {
+    console.log(this.logo);
+  }
+};
+</script>
---
+++ src/home/components/menubar.component.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="tab-filter-wrapper">
+  <div class="tab-filter">
+    <div class="filters">
+      <ul class="filters-list">
+        <li
+          v-for="category in categories"
+          :key="category"
+          @click="selectTab(category.category)">
+          <a :class="{'selected': category.selected}">{{category.category}}</a>
+        </li>
+        </ul>
+        <ul class="misc">
+          <li class="counter">
+            <a>{{counter}}</a>
+          </li>
+          <li :to="['/stats']">
+            <a>Stats</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['selectTab', 'counter', 'categories'],
+  name: 'HfMenubar'
+};
+</script>
