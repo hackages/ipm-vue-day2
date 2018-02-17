f876858b38d2760869c49f46ead027559d0ffd6d
Victor <victor@hackages.io>
2018-02-17 13:58:37 +0100

    feat(sidebar): integration

---
+++ src/home/components/sidebar.component.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="filter" :class="{'filter-is-visible':!toggle}">
+      <form @submit.prevent>
+        <div class="filter-block">
+          <h4>Search</h4>
+          <div class="filter-content">
+            <input  type="search" placeholder="title" :value="searchValue" @input="onSearch($event.target.value)"/>
+          </div>
+        </div>
+      </form>
+      <a class="hand-cursor close-f" @click="onClick(!toggle)" >Close</a>
+    </div>
+    <a class="hand-cursor filter-trigger" @click="onClick(!toggle)">
+      Filters
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HfSidebar',
+  props: {
+    onSearch: {
+      type: Function,
+      required: true
+    },
+    searchValue: {
+      type: String,
+      required: true
+    },
+    onClick: {
+      type: Function,
+      required: true
+    },
+    toggle: {
+      type: Boolean,
+      required: true
+    }
+  }
+};
+</script>
+
+
+
