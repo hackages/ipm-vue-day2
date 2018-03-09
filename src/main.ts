47461238c88de42659fe4c1de8bbd6a38feb7c44
Milenko <milenko@hackages.io>
2018-03-09 19:40:47 +0100

    feat(guards): Guard to forbid the access to the home and details page, and a guard to forbid the acc

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import App from './App.vue';
 import router from './router';
 import VeeValidate from 'vee-validate';
 import store from './core/state/store';
+import authenticationService from '@/core/authentication.service.ts';
 
 Vue.use(VeeValidate);
 
 Vue.config.productionTip = process.env.NODE_ENV === 'production';
-
+authenticationService.checkAuth();
 export default new Vue({
   render: h => h(App),
   router,
--- src/router/index.ts
+++ src/router/index.ts
@@ -4,22 +4,34 @@ import Router from 'vue-router';
 import HfHome from '@/home/home.component.vue';
 import HfAuthentication from '@/authentication/authentication.component.vue';
 import HfMovieDetail from '@/home/movie-details/movie-details.component.vue';
+import authenticationService from '@/core/authentication.service.ts';
+
+const authGuard = (to, from, next) => {
+  return authenticationService.user.authenticated ? next() : next('/auth');
+};
+
+const cannotGoOnAuthPageIfAuthenticatedGuard = (to, from, next) => {
+  return authenticationService.user.authenticated ? next('/home') : next();
+};
 
 const routes = [
   {
     path: '/auth',
     name: 'authentication',
     component: HfAuthentication,
+    beforeEnter: cannotGoOnAuthPageIfAuthenticatedGuard,
   },
   {
     path: '/home',
     name: 'home',
     component: HfHome,
+    beforeEnter: authGuard,
   },
   {
     path: '/details/:id',
     name: 'details',
     component: HfMovieDetail,
+    beforeEnter: authGuard,
   },
   {
     path: '**',
